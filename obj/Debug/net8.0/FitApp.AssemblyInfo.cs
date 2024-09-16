a8725ba61f3ac9b32fdab54571118c0644391479
Huba <hieu.dcu@gmail.com>
2024-09-16 22:31:07 +1200

    Update: CRUD-operations-icons

--- obj/Debug/net8.0/FitApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/FitApp.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bebe5a6587dcce452b465cc424d11b9f054a9cd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
