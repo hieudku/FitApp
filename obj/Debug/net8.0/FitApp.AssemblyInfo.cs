58e3dbd03a3d484bbcb7d7ed74631f739c738b64
Huba <hieu.dcu@gmail.com>
2024-10-01 02:44:18 +1300

    Update: home Index.cshtml

--- obj/Debug/net8.0/FitApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/FitApp.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdd6d8899e6b07d36ba6fafba2b38813d58aee5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9645cf22cd91d294510c821935e721600fc65c2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
