d12ecd7a60e76d10c599dbdcb0cc1ac0b14a114f
Huba <hieu.dcu@gmail.com>
2024-10-07 16:39:16 +1300

    Fix: data validation

--- FitAppSourceCode/Models/UserSpecificWorkout.cs
+++ FitAppSourceCode/Models/UserSpecificWorkout.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -9,22 +10,25 @@ namespace FitApp.Models
         [Key]
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "User ID is required.")]
         public string? UserId { get; set; }
 
         [ForeignKey("UserId")]
         public IdentityUser? User { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Workout Name is required.")]
+        [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters.")]
         public string? Name { get; set; }
 
+        [StringLength(500, ErrorMessage = "Description cannot exceed 500 characters.")]
         public string? Description { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Duration is required.")]
+        [Range(typeof(TimeSpan), "00:01:00", "05:00:00", ErrorMessage = "Duration must be between 1 minute and 5 hours.")]
         public TimeSpan Duration { get; set; }
 
-        [Required]
-        [Range(0, float.MaxValue)]
+        [Required(ErrorMessage = "Calories burned is required.")]
+        [Range(1, float.MaxValue, ErrorMessage = "Calories burned must be a positive number.")]
         public float CaloriesBurned { get; set; }
     }
 }
--- FitAppSourceCode/Models/Workouts.cs
+++ FitAppSourceCode/Models/Workouts.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FitApp.Models
@@ -6,9 +7,20 @@ namespace FitApp.Models
     public class Workouts
     {
         public int Id { get; set; }
-        public string  Name { get; set; }
-        public string  Description { get; set; }
+
+        [Required(ErrorMessage = "Workout Name is required.")]
+        [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters.")]
+        public string Name { get; set; }
+
+        [StringLength(500, ErrorMessage = "Description cannot exceed 500 characters.")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Duration is required.")]
+        [Range(typeof(TimeSpan), "00:01:00", "05:00:00", ErrorMessage = "Duration must be between 1 minute and 5 hours.")]
         public TimeSpan Duration { get; set; }
+
+        [Required(ErrorMessage = "Calories burned is required.")]
+        [Range(1, 10000, ErrorMessage = "Calories burned must be between 1 and 10,000.")]
         public float CaloriesBurned { get; set; }
     }
 }
